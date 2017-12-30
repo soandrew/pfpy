b8e5740a6dae1d20485d94afc764202e5daad4a8
Andrew So <soandrew@users.noreply.github.com>
2017-12-30 14:05:07 -0500

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pfpy",
+    version="1.0.0",
+    description="Add support for pointfree style programming in Python", 
+    packages=["pfpy"],
+    test_suite="tests"
+)
